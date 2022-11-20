5b71d7d7ef0f8a665a518e1497f586c3f7246e4b
tenessy0570 <tenessy0570@gmail.com>
2022-11-20 16:44:25 +0700

    registered models in admin

--- mailing/rest_api/admin.py
+++ mailing/rest_api/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from mailing.rest_api.models import Client
+from mailing.rest_api.models import Mailing
+from mailing.rest_api.models import Message
 # Register your models here.
+
+admin.register(Client)
+admin.register(Mailing)
+admin.register(Message)

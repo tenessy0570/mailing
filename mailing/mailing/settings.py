d902b582681589fa6705d4ce00618cbb411eba26
tenessy0570 <tenessy0570@gmail.com>
2022-12-11 02:04:23 +0700

    refactor

--- mailing/mailing/settings.py
+++ mailing/mailing/settings.py
@@ -26,7 +26,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_api.apps.RestApiConfig',
-    'scheduled_mailing.apps.ScheduledMailingConfig',
     'rest_framework'
 ]
 
--- mailing/scheduled_mailing/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScheduledMailingConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scheduled_mailing'
